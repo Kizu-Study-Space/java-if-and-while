1a795047de345a656776758ce4af509b0d057a1f
zorafuchs <13620981+zorafuchs@users.noreply.github.com>
2023-10-07 08:40:33 +0200

    Solve exercise

---
+++ src/main/java/point/Point.java
@@ -0,0 +1,17 @@
+package point;
+
+public class Point {
+    private int x;
+    private int y;
+
+
+    public Point () {
+        this(20,10);
+    }
+
+    public Point(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+}
