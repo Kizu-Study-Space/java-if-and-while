c9a80e8d375996a5c433ab3c60898cf989963c8d
zorafuchs <13620981+zorafuchs@users.noreply.github.com>
2023-10-10 17:20:23 +0200

    Test edge cases

--- src/test/java/number_comparer/NumberComparerTest.java
+++ src/test/java/number_comparer/NumberComparerTest.java
@@ -11,9 +11,14 @@ public class NumberComparerTest {
         NumberComparer numberComparer = new NumberComparer();
         assertEquals(5, numberComparer.max(3, 5));
         assertEquals(5, numberComparer.max(5, 3));
+        assertEquals(5, numberComparer.max(5, 5));
         assertEquals(5, numberComparer.max(5, 3, 2));
         assertEquals(5, numberComparer.max(3, 5, 2));
         assertEquals(5, numberComparer.max(2, 3, 5));
+        assertEquals(5, numberComparer.max(5, 3, 5));
+        assertEquals(5, numberComparer.max(5, 5, 3));
+        assertEquals(5, numberComparer.max(2, 5, 5));
+        assertEquals(5, numberComparer.max(5, 5, 5));
     }
     
 }
